d190781851927563ab212404525c04581e949399
yuanxx <824264439@qq.com>
2018-01-04 14:24:13 +0800

    Demo分支---excel处理
    Signed-off-by: yuanxx <824264439@qq.com>

---
+++ application/common/Hospital.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 2018/1/3
+ * Time: 23:25
+ */
+
+namespace app\common;
+
+
+use think\Model;
+
+class Hospital extends Model
+{
+    protected $table = 'hospital';
+}
---
+++ application/utils/PhpExcel.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 2018/1/3
+ * Time: 21:47
+ */
+class PhpExcelUtil
+{
+
+    public function getExcel()
+    {
+
+
+
+    }
+
+
+
+
+}
+
+
+?>
